a810c7f288fa3f00e3bc97ce1fff876327b746fd
Abhishek7668 <92740857+Abhishek7668@users.noreply.github.com>
2021-12-19 21:53:48 +0530

    Add files via upload

---
+++ simpe interset.py
@@ -0,0 +1,5 @@
+pricipal = int (input (" enter principal"))
+rate     = float ( input ( " enter rate of interest"))
+time     =  int ( input ( "enter time period"))
+simpleinterest = (pricipal*rate*time)/100
+print (" interest ",simpleinterest)
